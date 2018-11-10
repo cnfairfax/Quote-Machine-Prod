0a68204520f84c3eb6a7915594a60b40dd4f7457
Christopher Fairfax <cfairfax2010@gmail.com>
2018-11-10 10:28:51 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import WebFont from 'webfontloader';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import quote from './reducers/quote';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = combineReducers({quote});
+
+const store = createStore(
+    app,
+    applyMiddleware(thunkMiddleware)
+);
+
+window.store = store;
+
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
